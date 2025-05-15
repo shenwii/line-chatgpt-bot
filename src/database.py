2c50986731766cbc85bdd28a18b4855b87ad5f3b
shenwii <shinkosame@gmail.com>
2025-05-15 23:57:28 +0900

    vision機能追加

--- src/database.py
+++ src/database.py
@@ -9,27 +9,8 @@ class Database():
     def __init__(self, mongo_url: str, database_name: str) -> None:
         self.__client = AsyncIOMotorClient(mongo_url)
         self.__db = self.__client[database_name]
-        # self.__collection_assistant = self.__db["assistant"]
         self.__collection_user = self.__db["user"]
 
-    # async def fetch_all_assistant(self):
-    #     return self.__collection_assistant.find({})
-    
-    # async def fetch_assistant(self, key: str, model: str):
-    #     return await self.__collection_assistant.find_one({"key": key, "model": model})
-    
-    # async def insert_assistant(self, key: str, id: str, model:str, props: dict):
-    #     result = await self.__collection_assistant.insert_one({
-    #         "key": key,
-    #         "id": id,
-    #         "model": model,
-    #         "props": props
-    #     })
-    #     return result.inserted_id
-    
-    # async def delete_assistant(self, _id: str):
-    #     await self.__collection_assistant.delete_one({'_id': ObjectId(_id)})
-    
     async def fetch_user(self, id: str, default_assistant: str, default_model: str):
         user = await self.__collection_user.find_one({"id": id})
         if user:
--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@
 
 import openai
 import textwrap
+import io
+import asyncio
+import base64
 from urllib.parse import (
     urlencode,
     parse_qs
@@ -11,27 +14,32 @@ from database import Database
 from settings import Settings
 from command import Command
 from fastapi import Request, FastAPI, HTTPException, Depends
+from PIL import Image
 from linebot.v3 import (
     WebhookParser
 )
 from linebot.v3.messaging import (
     AsyncApiClient,
     AsyncMessagingApi,
+    AsyncMessagingApiBlob,
     Configuration,
     ReplyMessageRequest,
     TextMessage,
+    ImageMessage,
     TemplateMessage,
     CarouselTemplate,
     CarouselColumn,
     PostbackAction
 )
+
 from linebot.v3.exceptions import (
     InvalidSignatureError
 )
 from linebot.v3.webhooks import (
     MessageEvent,
     PostbackEvent,
-    TextMessageContent
+    TextMessageContent,
+    ImageMessageContent
 )
 import logging
 
@@ -45,12 +53,12 @@ __db = Database(app_settings.mongo_uri, app_settings.database)
 async def get_db():
     return __db
 
-async def get_linebot_api():
+async def get_linebot_client():
     configuration = Configuration(
         access_token = app_settings.line_channel_access_token
     )
     async with AsyncApiClient(configuration) as api_client:
-        yield AsyncMessagingApi(api_client)
+        yield api_client
 
 async def get_openai_client():
     async with openai.AsyncOpenAI() as openai_client:
@@ -59,20 +67,20 @@ async def get_openai_client():
 app = FastAPI()
 command = Command()
 
-async def handle_message(event = ..., linebot_api: AsyncMessagingApi = ..., db: Database = ..., openai_client: openai.AsyncOpenAI = ...):
+async def handle_message_text(event = ..., linebot_client: AsyncApiClient = ..., db: Database = ..., openai_client: openai.AsyncOpenAI = ...):
     user_id = event.source.user_id
     user = await db.fetch_user(user_id, next(iter(app_settings.assistants)), next(iter(app_settings.models)))
     if not user["model"] in app_settings.models:
-        await linebot_api.reply_message(
+        await AsyncMessagingApi(linebot_client).reply_message(
             ReplyMessageRequest(
                 reply_token = event.reply_token,
                 messages = [TextMessage(text=f"model is not exists: {user['model']}")]
             )
         )
         return
-    if app_settings.models[user["model"]]["type"] == "chat":
+    async def handle_chat():
         if not user["assistant"] in app_settings.assistants:
-            await linebot_api.reply_message(
+            await AsyncMessagingApi(linebot_client).reply_message(
                 ReplyMessageRequest(
                     reply_token = event.reply_token,
                     messages = [TextMessage(text=f"assistant is not exists: {user['assistant']}")]
@@ -81,18 +89,18 @@ async def handle_message(event = ..., linebot_api: AsyncMessagingApi = ..., db: 
             return
         conversation_history = user["conversation_history"][-app_settings.max_history:]
         if len(conversation_history) > 0 and type(conversation_history[-1]["content"]) == list:
-            conversation_history[-1]["content"].append({"type": "input_text", "text": event.message.text})
+            conversation_history[-1]["content"].insert(0, {"type": "input_text", "text": event.message.text})
         else:
             conversation_history.append({"role": "user", "content": event.message.text})
-        completion = await openai_client.chat.completions.create(
+        responses = await openai_client.responses.create(
             model = app_settings.models[user["model"]]["model"],
-            messages = [{"role": "system", "content": app_settings.assistants[user["assistant"]]["instructions"]}]
+            input = [{"role": "system", "content": app_settings.assistants[user["assistant"]]["instructions"]}]
                 + conversation_history,
             **app_settings.models[user["model"]]["props"]
         )
-        model_reply = completion.choices[0].message.content
+        model_reply = responses.output_text
         conversation_history.append({"role": "assistant", "content": model_reply})
-        await linebot_api.reply_message(
+        await AsyncMessagingApi(linebot_client).reply_message(
             ReplyMessageRequest(
                 reply_token = event.reply_token,
                 messages = [TextMessage(text=model_reply)]
@@ -100,19 +108,89 @@ async def handle_message(event = ..., linebot_api: AsyncMessagingApi = ..., db: 
         )
         await db.update_user(user["_id"], {"$set": {"conversation_history": conversation_history}})
 
-async def handle_action_select_model(event = ..., data = ..., linebot_api: AsyncMessagingApi = ..., db: Database = ..., openai_client: openai.AsyncOpenAI = ...):
+    if app_settings.models[user["model"]]["type"] == "chat":
+        await handle_chat()
+
+async def handle_message_image(event = ..., linebot_client: AsyncApiClient = ..., db: Database = ..., openai_client: openai.AsyncOpenAI = ...):
+    def compress_image(byte_data, max_pixel=1280, quality=30):
+        img = Image.open(io.BytesIO(byte_data))
+        calc_other_pixel = lambda a_pixel, b_pixel: int((max_pixel / a_pixel) * b_pixel)
+        width, height = img.size
+
+        if width > max_pixel or height > max_pixel:
+            if width > height:
+                img = img.resize((max_pixel, calc_other_pixel(width, height)))
+            else:
+                img = img.resize((calc_other_pixel(height, width), max_pixel))
+            
+        output = io.BytesIO()
+        img.save(output, format='JPEG', quality=quality)
+        with open("/tmp/test.jpg", 'wb') as f:
+            img.save(f, format='JPEG', quality=quality)
+        output.seek(0)
+        return base64.b64encode(output.read()).decode('utf-8')
+    
+    user_id = event.source.user_id
+    user = await db.fetch_user(user_id, next(iter(app_settings.assistants)), next(iter(app_settings.models)))
+    if not user["model"] in app_settings.models:
+        await AsyncMessagingApi(linebot_client).reply_message(
+            ReplyMessageRequest(
+                reply_token = event.reply_token,
+                messages = [TextMessage(text=f"model is not exists: {user['model']}")]
+            )
+        )
+        return
+    async def handle_chat():
+        if not app_settings.models[user["model"]]["vision"]:
+            await AsyncMessagingApi(linebot_client).reply_message(
+                ReplyMessageRequest(
+                    reply_token = event.reply_token,
+                    messages = [TextMessage(text=f"このもダルはvisionがサポートされていない：{user['model']}")]
+                )
+            )
+            return
+        message_content = await AsyncMessagingApiBlob(linebot_client).get_message_content(event.message.id)
+        base64_image = await asyncio.get_running_loop().run_in_executor(None, compress_image, message_content)
+        conversation_history = user["conversation_history"][-app_settings.max_history:]
+        if len(conversation_history) > 0 and type(conversation_history[-1]["content"]) == list:
+            conversation_history[-1]["content"].insert(0, {"type": "input_image", "image_url": f"data:image/jpeg;base64,{base64_image}"})
+        else:
+            conversation_history.append({"role": "user", "content": [{"type": "input_image", "image_url": f"data:image/jpeg;base64,{base64_image}"}]})
+        await db.update_user(user["_id"], {"$set": {"conversation_history": conversation_history}})
+        await AsyncMessagingApi(linebot_client).reply_message(
+            ReplyMessageRequest(
+                reply_token = event.reply_token,
+                messages = [TextMessage(text="この写真について、何が聞きたいですか。")]
+            )
+        )
+    if app_settings.models[user["model"]]["type"] == "chat":
+        await handle_chat()
+
+async def handle_action_select_model(event = ..., data = ..., linebot_client: AsyncApiClient = ..., db: Database = ..., openai_client: openai.AsyncOpenAI = ...):
     user_id = event.source.user_id
     user = await db.fetch_user(user_id, next(iter(app_settings.assistants)), next(iter(app_settings.models)))
     await db.update_user(user["_id"], {"$set": {"model": data["model"]}})
-    await linebot_api.reply_message(
+    await AsyncMessagingApi(linebot_client).reply_message(
         ReplyMessageRequest(
             reply_token = event.reply_token,
             messages = [TextMessage(text=f"モデル {data['model']} が選択しました。")]
         )
     )
 
+async def handle_action_select_assistant(event = ..., data = ..., linebot_client: AsyncApiClient = ..., db: Database = ..., openai_client: openai.AsyncOpenAI = ...):
+    user_id = event.source.user_id
+    user = await db.fetch_user(user_id, next(iter(app_settings.assistants)), next(iter(app_settings.models)))
+    await db.update_user(user["_id"], {"$set": {"assistant": data["assistant"]}})
+    await AsyncMessagingApi(linebot_client).reply_message(
+        ReplyMessageRequest(
+            reply_token = event.reply_token,
+            messages = [TextMessage(text=f"アシスタント {data['assistant']} が選択しました。")]
+        )
+    )
+
+
 @app.post("/callback")
-async def handle_callback(request: Request, linebot_api: AsyncMessagingApi = Depends(get_linebot_api), db: Database = Depends(get_db), openai_client: openai.AsyncOpenAI = Depends(get_openai_client)):
+async def handle_callback(request: Request, linebot_client: AsyncApiClient = Depends(get_linebot_client), db: Database = Depends(get_db), openai_client: openai.AsyncOpenAI = Depends(get_openai_client)):
     logger = logging.getLogger("handle_callback")
     signature = request.headers['X-Line-Signature']
     body = await request.body()
@@ -129,25 +207,24 @@ async def handle_callback(request: Request, linebot_api: AsyncMessagingApi = Dep
             data = parse_qs(event.postback.data)
             data = {k: v[0] for k, v in data.items()}
             if data["action"] == "select_model":
-                await handle_action_select_model(event, data, linebot_api, db, openai_client)
-            elif data["action"] == "xxx":
-                pass
+                await handle_action_select_model(event, data, linebot_client, db, openai_client)
+            elif data["action"] == "select_assistant":
+                await handle_action_select_assistant(event, data, linebot_client, db, openai_client)
             else:
-                await linebot_api.reply_message(
+                await AsyncMessagingApi(linebot_client).reply_message(
                     ReplyMessageRequest(
                         reply_token = event.reply_token,
                         messages = [TextMessage(text="未知アクション")]
                     )
                 )
         if isinstance(event, MessageEvent) and isinstance(event.message, TextMessageContent):
-            profile = await linebot_api.get_profile(event.source.user_id)
+            profile = await AsyncMessagingApi(linebot_client).get_profile(event.source.user_id)
             logger.info(f"{profile.display_name}（{profile.user_id}）からメッセージが届けました。")
-            print(profile.display_name)
             msg_txt = event.message.text
 
-            command_handle_rtn = await command.handle(msg_txt, linebot_api = linebot_api, db = db, event = event, openai_client = openai_client)
+            command_handle_rtn = await command.handle(msg_txt, linebot_client = linebot_client, db = db, event = event, openai_client = openai_client)
             if command_handle_rtn is None:
-                await linebot_api.reply_message(
+                await AsyncMessagingApi(linebot_client).reply_message(
                     ReplyMessageRequest(
                         reply_token = event.reply_token,
                         messages = [TextMessage(text="未知コマンド")]
@@ -156,7 +233,10 @@ async def handle_callback(request: Request, linebot_api: AsyncMessagingApi = Dep
                 continue
             if command_handle_rtn:
                 continue
-            await handle_message(event, linebot_api, db, openai_client)
+            await handle_message_text(event, linebot_client, db, openai_client)
+            continue
+        if isinstance(event, MessageEvent) and isinstance(event.message, ImageMessageContent):
+            await handle_message_image(event, linebot_client, db, openai_client)
             continue
     return 'OK'
 
@@ -165,9 +245,9 @@ async def health():
     return 'OK'
 
 @command.add("me")
-async def command_new(content, linebot_api: AsyncMessagingApi = ..., db: Database = ..., event = ..., openai_client: openai.AsyncOpenAI = ...):
-    profile = await linebot_api.get_profile(event.source.user_id)
-    await linebot_api.reply_message(
+async def command_new(content, linebot_client: AsyncApiClient = ..., db: Database = ..., event = ..., openai_client: openai.AsyncOpenAI = ...):
+    profile = await AsyncMessagingApi(linebot_client).get_profile(event.source.user_id)
+    await AsyncMessagingApi(linebot_client).reply_message(
         ReplyMessageRequest(
             reply_token = event.reply_token,
             messages = [TextMessage(text=f"名前：{profile.display_name}\nID：{profile.user_id}")]
@@ -175,7 +255,7 @@ async def command_new(content, linebot_api: AsyncMessagingApi = ..., db: Databas
     )
 
 @command.add("model")
-async def command_new(content, linebot_api: AsyncMessagingApi = ..., db: Database = ..., event = ..., openai_client: openai.AsyncOpenAI = ...):
+async def command_new(content, linebot_client: AsyncApiClient = ..., db: Database = ..., event = ..., openai_client: openai.AsyncOpenAI = ...):
     user_id = event.source.user_id
     user = await db.fetch_user(user_id, next(iter(app_settings.assistants)), next(iter(app_settings.models)))
 
@@ -190,7 +270,7 @@ async def command_new(content, linebot_api: AsyncMessagingApi = ..., db: Databas
         )
         columns.append(column)
 
-    await linebot_api.reply_message(
+    await AsyncMessagingApi(linebot_client).reply_message(
         ReplyMessageRequest(
             reply_token = event.reply_token,
             messages = [TemplateMessage(
@@ -201,19 +281,41 @@ async def command_new(content, linebot_api: AsyncMessagingApi = ..., db: Databas
             )]
         )
     )
-    
-
 
 @command.add("assistant")
-async def command_new(content, linebot_api: AsyncMessagingApi = ..., db: Database = ..., event = ..., openai_client: openai.AsyncOpenAI = ...):
-    pass
+async def command_new(content, linebot_client: AsyncApiClient = ..., db: Database = ..., event = ..., openai_client: openai.AsyncOpenAI = ...):
+    user_id = event.source.user_id
+    user = await db.fetch_user(user_id, next(iter(app_settings.assistants)), next(iter(app_settings.models)))
+
+    columns = []
+    for key, value in app_settings.assistants.items():
+        column = CarouselColumn(
+            text = f"{'**' if user['assistant'] == key else ''}アシスタント：{key}\n指示：{value['instructions']}\n説明：{value['description']}",
+            actions = [PostbackAction(label="選択", data=urlencode({
+                "action": "select_assistant",
+                "assistant": key
+            }))]
+        )
+        columns.append(column)
+
+    await AsyncMessagingApi(linebot_client).reply_message(
+        ReplyMessageRequest(
+            reply_token = event.reply_token,
+            messages = [TemplateMessage(
+                alt_text = "アシスタントを選択してください。",
+                template = CarouselTemplate(
+                    columns = columns
+                )
+            )]
+        )
+    )
 
 @command.add("new")
-async def command_new(content, linebot_api: AsyncMessagingApi = ..., db: Database = ..., event = ..., openai_client: openai.AsyncOpenAI = ...):
+async def command_new(content, linebot_client: AsyncApiClient = ..., db: Database = ..., event = ..., openai_client: openai.AsyncOpenAI = ...):
     user_id = event.source.user_id
     user = await db.fetch_user(user_id, next(iter(app_settings.assistants)), next(iter(app_settings.models)))
     await db.update_user(user["_id"], {"$set": {"conversation_history": []}})
-    await linebot_api.reply_message(
+    await AsyncMessagingApi(linebot_client).reply_message(
         ReplyMessageRequest(
             reply_token = event.reply_token,
             messages = [TextMessage(text="セッションがクリアしました。")]
@@ -221,8 +323,8 @@ async def command_new(content, linebot_api: AsyncMessagingApi = ..., db: Databas
     )
 
 @command.add("help")
-async def command_help(content, linebot_api: AsyncMessagingApi = ..., db: Database = ..., event = ..., openai_client: openai.AsyncOpenAI = ...):
-    await linebot_api.reply_message(
+async def command_help(content, linebot_client: AsyncApiClient = ..., db: Database = ..., event = ..., openai_client: openai.AsyncOpenAI = ...):
+    await AsyncMessagingApi(linebot_client).reply_message(
         ReplyMessageRequest(
             reply_token = event.reply_token,
             messages = [TextMessage(text=textwrap.dedent("""
